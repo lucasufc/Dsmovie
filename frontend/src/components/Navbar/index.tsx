cba4cc64b263415eec484d30092af20aafe66a97
Lucas Martins <lucashk.eng@gmail.com>
2022-01-10 19:56:00 -0300

    refact:  navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import './Navbar.css'
+export const Navbar = () => {
+    return(
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/lucasufc" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p>/lucasufc</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
